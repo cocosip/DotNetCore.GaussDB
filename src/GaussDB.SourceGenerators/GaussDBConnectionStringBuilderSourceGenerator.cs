cebc82ec79f2f6db79e58ec17a3e53c84fd024c5
virtual <1185513330@qq.com>
2025-02-07 17:36:55 +0800

    upgrade package and add net9.0
    Signed-off-by: virtual <1185513330@qq.com>

--- src/GaussDB.SourceGenerators/GaussDBConnectionStringBuilderSourceGenerator.cs
+++ src/GaussDB.SourceGenerators/GaussDBConnectionStringBuilderSourceGenerator.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Scriban;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
-using Scriban;
 
 namespace GaussDB.SourceGenerators;
 
 [Generator]
-public class GaussDBConnectionStringBuilderSourceGenerator : ISourceGenerator
+public class GaussDBConnectionStringBuilderSourceGenerator : IIncrementalGenerator
 {
     static readonly DiagnosticDescriptor InternalError = new DiagnosticDescriptor(
         id: "PGXXXX",
@@ -19,106 +19,114 @@ public class GaussDBConnectionStringBuilderSourceGenerator : ISourceGenerator
         DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
-    public void Initialize(GeneratorInitializationContext context) { }
-
-    public void Execute(GeneratorExecutionContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        if (context.Compilation.Assembly.GetTypeByMetadataName("GaussDB.GaussDBConnectionStringBuilder") is not { } type)
-            return;
+        // 获取 GaussDBConnectionStringBuilder 类型
+        var gaussDBConnectionStringBuilderType = context.CompilationProvider
+            .Select((compilation, _) => compilation.Assembly.GetTypeByMetadataName("GaussDB.GaussDBConnectionStringBuilder"));
 
-        if (context.Compilation.Assembly.GetTypeByMetadataName("GaussDB.GaussDBConnectionStringPropertyAttribute") is not
-            { } connectionStringPropertyAttribute)
+        // 获取 GaussDBConnectionStringPropertyAttribute 类型
+        var connectionStringPropertyAttributeType = context.CompilationProvider
+            .Select((compilation, _) => compilation.Assembly.GetTypeByMetadataName("GaussDB.GaussDBConnectionStringPropertyAttribute"));
+
+        // 获取其他必要的属性类型
+        var obsoleteAttributeType = context.CompilationProvider
+            .Select((compilation, _) => compilation.GetTypeByMetadataName("System.ObsoleteAttribute"));
+        var displayNameAttributeType = context.CompilationProvider
+            .Select((compilation, _) => compilation.GetTypeByMetadataName("System.ComponentModel.DisplayNameAttribute"));
+        var defaultValueAttributeType = context.CompilationProvider
+            .Select((compilation, _) => compilation.GetTypeByMetadataName("System.ComponentModel.DefaultValueAttribute"));
+
+        // 组合所有必要的信息
+        var combined = gaussDBConnectionStringBuilderType
+            .Combine(connectionStringPropertyAttributeType)
+            .Combine(obsoleteAttributeType)
+            .Combine(displayNameAttributeType)
+            .Combine(defaultValueAttributeType);
+
+        // 注册生成逻辑
+        context.RegisterSourceOutput(combined, (spc, source) =>
         {
-            context.ReportDiagnostic(Diagnostic.Create(
-                InternalError,
-                location: null,
-                "Could not find GaussDB.GaussDBConnectionStringPropertyAttribute"));
-            return;
-        }
+            var ((((type, connectionStringPropertyAttribute), obsoleteAttribute), displayNameAttribute), defaultValueAttribute) = source;
 
-        var obsoleteAttribute = context.Compilation.GetTypeByMetadataName("System.ObsoleteAttribute");
-        var displayNameAttribute = context.Compilation.GetTypeByMetadataName("System.ComponentModel.DisplayNameAttribute");
-        var defaultValueAttribute = context.Compilation.GetTypeByMetadataName("System.ComponentModel.DefaultValueAttribute");
-
-        if (obsoleteAttribute is null || displayNameAttribute is null || defaultValueAttribute is null)
-        {
-            context.ReportDiagnostic(Diagnostic.Create(
-                InternalError,
-                location: null,
-                "Could not find ObsoleteAttribute, DisplayNameAttribute or DefaultValueAttribute"));
-            return;
-        }
-
-        var properties = new List<PropertyDetails>();
-        var propertiesByKeyword = new Dictionary<string, PropertyDetails>();
-        foreach (var member in type.GetMembers())
-        {
-            if (member is not IPropertySymbol property ||
-                property.GetAttributes().FirstOrDefault(a => connectionStringPropertyAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default)) is not { } propertyAttribute ||
-                property.GetAttributes()
-                    .FirstOrDefault(a => displayNameAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default))
-                    ?.ConstructorArguments[0].Value is not string displayName)
+            if (type is null || connectionStringPropertyAttribute is null || obsoleteAttribute is null || displayNameAttribute is null || defaultValueAttribute is null)
             {
-                continue;
+                spc.ReportDiagnostic(Diagnostic.Create(
+                    InternalError,
+                    location: null,
+                    "Could not find necessary types or attributes"));
+                return;
             }
 
-            var explicitDefaultValue = property.GetAttributes()
-                .FirstOrDefault(a => defaultValueAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default))
-                ?.ConstructorArguments[0].Value;
-
-            if (explicitDefaultValue is string s)
-                explicitDefaultValue = '"' + s.Replace("\"", "\"\"") + '"';
-
-            if (explicitDefaultValue is not null && property.Type.TypeKind == TypeKind.Enum)
+            var properties = new List<PropertyDetails>();
+            var propertiesByKeyword = new Dictionary<string, PropertyDetails>();
+            foreach (var member in type.GetMembers())
             {
-                explicitDefaultValue = $"({property.Type.Name}){explicitDefaultValue}";
-                // var foo = property.Type.Name;
-                // explicitDefaultValue += $"/* {foo} */";
-            }
-
-            var propertyDetails = new PropertyDetails
-            {
-                Name = property.Name,
-                CanonicalName = displayName,
-                TypeName = property.Type.Name,
-                IsEnum = property.Type.TypeKind == TypeKind.Enum,
-                IsObsolete = property.GetAttributes().Any(a => obsoleteAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default)),
-                DefaultValue = explicitDefaultValue
-            };
-
-            properties.Add(propertyDetails);
-
-            propertiesByKeyword[displayName.ToUpperInvariant()] = propertyDetails;
-            if (property.Name != displayName)
-            {
-                var propertyName = property.Name.ToUpperInvariant();
-                if (!propertiesByKeyword.ContainsKey(propertyName))
-                    propertyDetails.Alternatives.Add(propertyName);
-            }
-
-            if (propertyAttribute.ConstructorArguments.Length == 1)
-            {
-                foreach (var synonymArg in propertyAttribute.ConstructorArguments[0].Values)
+                if (member is not IPropertySymbol property ||
+                    property.GetAttributes().FirstOrDefault(a => connectionStringPropertyAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default)) is not { } propertyAttribute ||
+                    property.GetAttributes()
+                        .FirstOrDefault(a => displayNameAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default))
+                        ?.ConstructorArguments[0].Value is not string displayName)
                 {
-                    if (synonymArg.Value is string synonym)
+                    continue;
+                }
+
+                var explicitDefaultValue = property.GetAttributes()
+                    .FirstOrDefault(a => defaultValueAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default))
+                    ?.ConstructorArguments[0].Value;
+
+                if (explicitDefaultValue is string s)
+                    explicitDefaultValue = '"' + s.Replace("\"", "\"\"") + '"';
+
+                if (explicitDefaultValue is not null && property.Type.TypeKind == TypeKind.Enum)
+                {
+                    explicitDefaultValue = $"({property.Type.Name}){explicitDefaultValue}";
+                }
+
+                var propertyDetails = new PropertyDetails
+                {
+                    Name = property.Name,
+                    CanonicalName = displayName,
+                    TypeName = property.Type.Name,
+                    IsEnum = property.Type.TypeKind == TypeKind.Enum,
+                    IsObsolete = property.GetAttributes().Any(a => obsoleteAttribute.Equals(a.AttributeClass, SymbolEqualityComparer.Default)),
+                    DefaultValue = explicitDefaultValue
+                };
+
+                properties.Add(propertyDetails);
+
+                propertiesByKeyword[displayName.ToUpperInvariant()] = propertyDetails;
+                if (property.Name != displayName)
+                {
+                    var propertyName = property.Name.ToUpperInvariant();
+                    if (!propertiesByKeyword.ContainsKey(propertyName))
+                        propertyDetails.Alternatives.Add(propertyName);
+                }
+
+                if (propertyAttribute.ConstructorArguments.Length == 1)
+                {
+                    foreach (var synonymArg in propertyAttribute.ConstructorArguments[0].Values)
                     {
-                        var synonymName = synonym.ToUpperInvariant();
-                        if (!propertiesByKeyword.ContainsKey(synonymName))
-                            propertyDetails.Alternatives.Add(synonymName);
+                        if (synonymArg.Value is string synonym)
+                        {
+                            var synonymName = synonym.ToUpperInvariant();
+                            if (!propertiesByKeyword.ContainsKey(synonymName))
+                                propertyDetails.Alternatives.Add(synonymName);
+                        }
                     }
                 }
             }
-        }
 
-        var template = Template.Parse(EmbeddedResource.GetContent("GaussDBConnectionStringBuilder.snbtxt"), "GaussDBConnectionStringBuilder.snbtxt");
+            var template = Template.Parse(EmbeddedResource.GetContent("GaussDBConnectionStringBuilder.snbtxt"), "GaussDBConnectionStringBuilder.snbtxt");
 
-        var output = template.Render(new
-        {
-            Properties = properties,
-            PropertiesByKeyword = propertiesByKeyword
+            var output = template.Render(new
+            {
+                Properties = properties,
+                PropertiesByKeyword = propertiesByKeyword
+            });
+
+            spc.AddSource(type.Name + ".Generated.cs", SourceText.From(output, Encoding.UTF8));
         });
-
-        context.AddSource(type.Name + ".Generated.cs", SourceText.From(output, Encoding.UTF8));
     }
 
     sealed class PropertyDetails
